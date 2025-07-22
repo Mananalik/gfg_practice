4ca7459fe6e9a1d197a032f9d7a55dd5a16f7dc7
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2025-07-22 21:59:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -22,28 +22,40 @@ class Solution {
         int vis[] = new int[V];
         for(int i=0;i<V;i++){
             if(vis[i]==0){
-                if(func(i,adj,vis)) return true;
+                if(func(-1,i,adj,vis)) return true;
             }
         }
        return false;
     }
-    static boolean func(int src,ArrayList<ArrayList<Integer>> adj,int vis[]){
-         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(-1,src));
-        while(!q.isEmpty()){
-            int parent = q.peek().first;
-            int child = q.peek().second;
-            q.remove();
-            vis[child] = 1;
-            for(int i: adj.get(child)){
-                if(vis[i]==0){
-                    q.add(new Pair(child,i));
-                }
-                else if(parent!=i){
+    static boolean func(int parent,int child, ArrayList<ArrayList<Integer>> adj,int vis[]){
+        vis[child] = 1;
+        for(int i: adj.get(child)){
+            if(vis[i]==0){
+                if(func(child,i,adj,vis)){
                     return true;
                 }
             }
+            else if(parent!=i){
+                return true;
+            }
         }
         return false;
+        //  Queue<Pair> q = new LinkedList<>();
+        // q.add(new Pair(-1,src));
+        // while(!q.isEmpty()){
+        //     int parent = q.peek().first;
+        //     int child = q.peek().second;
+        //     q.remove();
+        //     vis[child] = 1;
+        //     for(int i: adj.get(child)){
+        //         if(vis[i]==0){
+        //             q.add(new Pair(child,i));
+        //         }
+        //         else if(parent!=i){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
     }
 }
